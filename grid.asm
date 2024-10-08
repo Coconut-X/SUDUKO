4d080f50405b16e7a71b075213afc8b5c000a637
Coconut-X <aliimranqureshi768@gmail.com>
2024-10-08 22:55:07 +0500

    bitmapped numbers of 7x7

--- grid.asm
+++ grid.asm
@@ -2,6 +2,80 @@
 
 jmp start
 
+
+zero_bitmap:    db 0,1,1,1,1,1,0,   1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  0,1,1,1,1,1,0,  2 ;7x7 bitmap , 7 is height of the bitmap
+
+one_bitmap:     db 0,0,0,1,0,0,0,   0,0,1,1,0,0,0,  0,1,0,1,0,0,0,  0,0,0,1,0,0,0,  0,0,0,1,0,0,0,  0,0,0,1,0,0,0,  0,1,1,1,1,1,0,  2 ;7x7 bitmap
+
+two_bitmap:     db 1,1,1,1,1,1,1,   0,0,0,0,0,0,1,  0,0,0,0,0,0,1,  1,1,1,1,1,1,1,  1,0,0,0,0,0,0,  1,0,0,0,0,0,0,  1,1,1,1,1,1,1,  2 ;
+
+three_bitmap:   db 1,1,1,1,1,1,1,   0,0,0,0,0,0,1,  0,0,0,0,0,0,1,  1,1,1,1,1,1,1,  0,0,0,0,0,0,1,  0,0,0,0,0,0,1,  1,1,1,1,1,1,1,  2 ;
+
+four_bitmap:    db 1,0,0,0,0,0,1,   1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  1,1,1,1,1,1,1,  0,0,0,0,0,0,1,  0,0,0,0,0,0,1,  0,0,0,0,0,0,1,  2 ;
+
+five_bitmap:    db 1,1,1,1,1,1,1,   1,0,0,0,0,0,0,  1,0,0,0,0,0,0,  1,1,1,1,1,1,1,  0,0,0,0,0,0,1,  0,0,0,0,0,0,1,  1,1,1,1,1,1,1,  2 ;
+
+six_bitmap:     db 1,1,1,1,1,1,1,   1,0,0,0,0,0,0,  1,0,0,0,0,0,0,  1,1,1,1,1,1,1,  1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  1,1,1,1,1,1,1,  2 ;
+
+seven_bitmap:   db 1,1,1,1,1,1,1,   0,0,0,0,0,1,0,  0,0,0,0,1,0,0,  0,0,0,1,0,0,0,  0,0,1,0,0,0,0,  0,1,0,0,0,0,0,  1,0,0,0,0,0,0,  2 ;
+
+eight_bitmap:   db 1,1,1,1,1,1,1,   1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  1,1,1,1,1,1,1,  1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  1,1,1,1,1,1,1,  2 ;
+
+nine_bitmap:    db 1,1,1,1,1,1,1,   1,0,0,0,0,0,1,  1,0,0,0,0,0,1,  1,1,1,1,1,1,1,  0,0,0,0,0,0,1,  0,0,0,0,0,0,1,  1,1,1,1,1,1,1,  2 ;
+
+
+
+
+setDI_One:
+    mov di,1
+    add dx,1 ;increment y axis
+    int 10h
+    sub cx,5 ;decrement x axis to original position
+    ret
+
+DRAW_BITMAP:
+    push bp
+    mov bp,sp
+    pushA
+    ;mov si,zero_bitmap ;the array will be in bx
+    mov si, nine_bitmap
+
+    mov ah, 0x0c
+    mov bh, 0
+    mov cx, [bp+4] ;x
+    mov dx, [bp+6] ;y
+    dec dx
+    mov al, 12
+
+
+    nextRow:
+        inc dx
+        mov di, 1
+        mov cx, [bp + 4]
+        CurrentRow:
+            cmp byte[si], 2
+            jz exitDrawBitmap
+            cmp byte[si], 1
+            jnz skipPrint
+            int 10h
+            skipPrint:
+                inc cx
+                inc di
+                inc si
+                cmp di, 8
+            jz nextRow
+        jmp CurrentRow
+
+
+
+    exitDrawBitmap:
+        popA
+        mov sp,bp
+        pop bp
+
+ret 4
+
+
 clear_screen:
     mov ah, 0x06       ; Scroll up function
     mov al, 0          ; Number of lines to scroll (0 = clear entire screen)
@@ -12,72 +86,145 @@ clear_screen:
     ret                ; Return from subroutine
 
 
+
 drawVertical:
     mov ah, 0x0c
     push bp
     mov bp, sp
-    sub sp, 2
+    sub sp, 2 ;space for counter
     pushA
     mov si, [bp + 4] ; counter
     mov di, 0
-    mov cx, [bp + 8]
-    mov dx, [bp + 6]
+    mov cx, [bp + 8] ;x
+    mov dx, [bp + 6] ;y
     mov bh, 0
     mov al, 12
 
-    reloop:
+    loopDrawVertical:
         int 10h
-        inc dx
-        dec si
-    jnz reloop
+        inc dx ;increment y axis
+        dec si ;decrement counter
+    jnz loopDrawVertical
+
     popA
     mov sp, bp
     pop bp
 ret 6
+
+
+DRAW_VERTICAL_GRID:
+
+    push bp
+    mov bp, sp
+
+    mov bx,8
+    push bx ;x
+    push word 50 ;y
+    push word 405 ;len
+
+    mov cx,10
+
+    loopDrawVericalGrid:
+
+        call drawVertical
+            add bx, 45
+            push bx ;x
+            push word 50 ;y
+            push word 405 ;len
+
+            loop loopDrawVericalGrid
+
+
+    mov sp, bp
+    pop bp
+ret
+
 
 
 drawHorizontal:
     mov ah, 0x0c
     push bp
     mov bp, sp
-    sub sp, 2
+    sub sp, 2 ;space for counter
     pushA
-    mov si, [bp + 4] ; counter
-    mov di, 0
-    mov cx, [bp + 8]
-    mov dx, [bp + 6]
-    mov bh, 0
-    mov al, 12
 
-    reloop1:
+    mov si, [bp + 4] ; counter
+    mov cx, [bp + 8] ;x
+    mov dx, [bp + 6] ;y
+    mov bh, 0 ;page number
+    mov al, 12 ;color
+
+    loopDrawHorizontal:
         int 10h
-        inc cx
-        dec si
-    jnz reloop1
+        inc cx ;increment x axis
+        dec si ;decrement counter
+    jnz loopDrawHorizontal
     popA
     mov sp, bp
     pop bp
 ret 6
 
 
+
+
+
+DRAW_HORIZONTAL_GRID:
+    push bp
+    mov bp, sp
+    mov bx,50
+    push word 8 ;x
+    push bx ;y
+    push word 405 ;len
+
+    mov cx,10
+
+    loopDrawHorizontal_Grid:
+        call drawHorizontal
+            add bx, 45
+            push word 8 ;x
+            push bx ;y
+            push word 405 ;len
+
+            loop loopDrawHorizontal_Grid
+
+    mov sp, bp
+    pop bp
+    ret 
+
+
+DRAW_THICK_VERTICAL:
+
+ret
+
+
+DRAW_THICK_HORIZONTAL:
+
+ret
+
+
+
+
+
 start:
     call clear_screen
     
-    mov ax, 0x13
+    mov ax, 0x12
     int 10h
+   
+    ;call DRAW_HORIZONTAL_GRID
 
-    push word 150 ;x
-    push word 150 ;y
-    push word 50 ;len
-    call drawVertical
+    ;call DRAW_VERTICAL_GRID
 
-    push word 150 ;x
-    push word 150 ;y
-    push word 50 ;len
-    call drawHorizontal
+   
+    ; push word 50 ;y
+    ; push word 50 ;x
 
-    mov ax, 0x4c00
-    int 21h
+    ; call DRAW_BITMAP
+
+
+
+mov ax, 0x4c00
+int 21h
 
 
 
